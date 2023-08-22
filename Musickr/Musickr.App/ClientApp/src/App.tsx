0ed643a656a6b850081f736fab8879927774b497
LouisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2023-08-22 17:28:37 +0200

    React Query added

--- Musickr/Musickr.App/ClientApp/src/App.tsx
+++ Musickr/Musickr.App/ClientApp/src/App.tsx
@@ -4,19 +4,23 @@ import AppRoutes from './AppRoutes';
 import Layout from './components/Layout';
 import './custom.css';
 import {ChakraProvider} from "@chakra-ui/react";
+import {QueryClient, QueryClientProvider} from "react-query";
 
+const queryClient = new QueryClient();
 const App : React.FC = () => {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, ...rest } = route;
+              return <Route key={index} {...rest} element={element} />;
+            })}
+          </Routes>
+        </Layout>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 };
 
