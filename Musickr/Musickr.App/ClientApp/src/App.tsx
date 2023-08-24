407f63b6ddf90707fd0ee8be3510b81238f0b683
DeerisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2023-08-24 16:59:25 +0200

    Components removed

--- Musickr/Musickr.App/ClientApp/src/App.tsx
+++ Musickr/Musickr.App/ClientApp/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
-import Layout from './components/Layout';
 import './custom.css';
 import {ChakraProvider} from "@chakra-ui/react";
 import {QueryClient, QueryClientProvider} from "react-query";
@@ -11,14 +10,12 @@ const App : React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <Layout>
-          <Routes>
-            {AppRoutes.map((route, index) => {
-              const { element, ...rest } = route;
-              return <Route key={index} {...rest} element={element} />;
-            })}
-          </Routes>
-        </Layout>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element} />;
+          })}
+        </Routes>
       </ChakraProvider>
     </QueryClientProvider>
   );
