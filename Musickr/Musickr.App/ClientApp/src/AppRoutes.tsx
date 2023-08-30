198d118dabe8a94009ab3258b6b3af0b6eeca84d
DeerisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2023-08-30 22:32:39 +0200

    AppRoutes.tsx updated

--- Musickr/Musickr.App/ClientApp/src/AppRoutes.tsx
+++ Musickr/Musickr.App/ClientApp/src/AppRoutes.tsx
@@ -1,9 +1,14 @@
 import SearchPage from "./components/Search/Pages/SearchPage";
+import PlayerPage from "./components/Player/Pages/PlayerPage";
 
 const AppRoutes = [
   {
     index: true,
     element: <SearchPage />
+  },
+  {
+    path: "/player",
+    element: <PlayerPage />
   }
 ];
 
